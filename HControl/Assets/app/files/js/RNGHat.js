da1107c1327227167d992657ff872832c43c3624
Jeff Sweeney <jeff@tybern.com>
2026-02-08 19:00:31 +1000

    Cleanup old JS

--- HControl/Assets/app/files/js/RNGHat.js
+++
@@ -1,7 +0,0 @@
-// RNG that acts like picking numbers out of a hat, and putting them back in the hat immediately.
-var RNGHat = {
-	getRand: function(max)
-	{
-		return Math.floor(Math.random() * (max + 1));
-	}
-};
